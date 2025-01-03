1b02812e76f0b1e5fbbb24e09a21710351dca933
Hayden <hayden@haydenzeller.com>
2025-01-03 16:39:57 -0500

    rm links

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+// import Link from "next/link";
 import Footer from "./components/Footer";
 
 export default function Home() {
@@ -10,20 +10,14 @@ export default function Home() {
           Computer science student from{" "}
           <span className=" text-red-500">Canada</span>.
         </p>
-        <div className="flex flex-row mt-1 gap-5">
+        {/* <div className="flex flex-row mt-1 gap-5">
           <Link
             href="https://x.com/haydendevs"
             className="link hover:text-red-500 text-xl font-bold"
           >
             X
           </Link>
-          <Link
-            href="https://github.com/haydenzeller"
-            className="link hover:text-red-500 text-xl font-bold"
-          >
-            Github
-          </Link>
-        </div>
+        </div> */}
       </section>
       <section className="max-w-3xl mx-auto px-5 mt-5"></section>
       <Footer />
