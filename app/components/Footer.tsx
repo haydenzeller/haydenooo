e905a139093a1a6ef6c5423ad26211c77ff5d501
Hayden <hayden@haydenzeller.com>
2025-01-04 15:51:47 -0500

    location

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,9 +1,15 @@
-"use client"
+"use client";
 import { useState, useEffect } from "react";
 
 export default function Footer() {
   const [count, setCount] = useState(0);
+  const [location, setLocation] = useState("");
 
+  const getLocation = async () => {
+    const response = await fetch("https://geolocation-db.com/json/");
+    const data = await response.json();
+    setLocation(data.country_name);
+  };
   const getCount = async () => {
     const response = await fetch(
       "https://abacus.jasoncameron.dev/hit/hayden.ooo/visits"
@@ -14,13 +20,22 @@ export default function Footer() {
 
   useEffect(() => {
     getCount();
+    getLocation();
   }, []);
 
   return (
     <div className="absolute bottom-0 w-full p-5">
-      <div className="flex flex-row gap-2 justify-center items-center">
-        <div className="w-3 h-3 bg-green-500 rounded-full animate-pulse"></div>
-        <div className="font-bold">{count} Visitors</div>
+      <div className="flex flex-row justify-center items-center gap-5">
+        <div className="flex flex-row gap-2 justify-center items-center">
+          <div className="w-3 h-3 bg-green-500 rounded-full animate-pulse"></div>
+          <div className="font-bold">{count} Visits</div>
+        </div>
+        <div className="font-bold text-center justify-center items-center">
+          <p className="">
+            Hello stranger from <span className="text-red-500">{location}</span>
+            .
+          </p>
+        </div>
       </div>
     </div>
   );
